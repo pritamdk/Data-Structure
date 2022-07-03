5e3aebe0f8ad3a98bff37c2eb25311a8e6261c86
pritamdk <pritamdk1994@gmail.com>
2022-07-04 00:25:12 +0530

    Added : UC1- Created Linked List

--- com/datastructure/MyNode.java
+++ com/datastructure/MyNode.java
@@ -1,10 +1,32 @@
 package com.datastructure;
 
-public class MyNode {
+public class MyNode<K>
+{
+    private K key;
+    private MyNode next;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List");
+    public MyNode(K key) {
 
-	}
+        this.key = key;
+        this.next = null;
+    }
 
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("----- Welcome to Data Structure: Linked List -----");
+
+        MyNode<Integer> firstNode = new MyNode<Integer>(56);
+        MyNode<Integer> secondNode = new MyNode<Integer>(30);
+        MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+    }
 }
